496eb2db682305b9fe8dddba5e5bdc6ff394652a
smissin <smissin@nortech.gr>
2018-07-24 21:43:45 +0300

    fixed email column on register form

--- LiveSite/user/register.php
+++ LiveSite/user/register.php
@@ -3,8 +3,8 @@
 require_once 'config.php';
  
 // Define variables and initialize with empty values
-$username = $password = $confirm_password = "";
-$username_err = $password_err = $confirm_password_err = "";
+$username = $email =  $password = $confirm_password = "";
+$username_err = $email_err = $password_err = $confirm_password_err = "";
  
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -41,6 +41,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         // Close statement
         mysqli_stmt_close($stmt);
     }
+
+        // Validate Email
+        if(empty(trim($_POST['email']))){
+            $email_err = "Please enter an email.";     
+        } else{
+            $email = trim($_POST['email']);
+        }
     
     // Validate password
     if(empty(trim($_POST['password']))){
@@ -65,14 +72,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
         
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $sql = "INSERT INTO users (username, email, password) VALUES (?,?, ?)";
          
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+            mysqli_stmt_bind_param($stmt, "sss", $param_username,$param_email, $param_password);
             
             // Set parameters
             $param_username = $username;
+            $param_email = $email;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
             
             // Attempt to execute the prepared statement
@@ -133,6 +141,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
 							<i class="fa fa-user"></i>
+						</span>
+                    </div>
+
+                    <div class="wrap-input100 validate-input m-b-10" data-validate = "Email is required">
+						<input class="input100" type="text" name="email" placeholder="Email" value="<?php echo $email; ?>">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope"></i>
 						</span>
 					</div>
 
