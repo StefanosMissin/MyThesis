32e9f14c555a50ba77efa1d87160f24f1327e362
smissin <smissin@nortech.gr>
2018-10-16 21:30:48 +0300

    disabled search button on LiveSite

--- LiveSite/dashboard/static/upperbody.php
+++ LiveSite/dashboard/static/upperbody.php
@@ -51,7 +51,7 @@
                         <!-- ============================================================== -->
                         <li class="nav-item hidden-sm-down search-box">
                             <a class="nav-link hidden-sm-down text-muted waves-effect waves-dark" href="javascript:void(0)">
-                                <i class="ti-search"></i>
+                                <i class="ti-search disp-not"></i>
                             </a>
                             <form class="app-search">
                                 <input type="text" class="form-control" placeholder="Search & enter">
