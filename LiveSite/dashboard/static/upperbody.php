457f084b7a0c25659b528ed5767a18af8c0c9dfe
smissin <smissin@nortech.gr>
2018-10-17 19:24:16 +0300

    Finished chapter 5 on Paper/Missin Stefanos Diploma Thesis.docx

--- LiveSite/dashboard/static/upperbody.php
+++ LiveSite/dashboard/static/upperbody.php
@@ -259,7 +259,7 @@
                                 <form>
                                     <div class="form-group">
                                         <label for="recipient-name" class="col-form-label">Recipient:</label>
-                                        <input type="text" class="form-control" id="recipient-name">
+                                        <input type="text" class="form-control" id="recipient-name" readonly>
                                     </div>
                                     <div class="form-group">
                                         <label for="message-text" class="col-form-label">Message:</label>
